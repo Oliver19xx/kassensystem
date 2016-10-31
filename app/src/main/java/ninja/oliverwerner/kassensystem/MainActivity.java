d867f248a00fb071ea25d76a09e396413b9649b3
Oliver19xx <oliver.werner94@gmail.com>
2016-10-31 09:42:59 +0100

    - GridView eingefügt für die Auflistung der Buttons - Function generateButtons() geupdatet (Codeübernahme aus Übungsprojekt)

--- app/src/main/java/ninja/oliverwerner/kassensystem/MainActivity.java
+++ app/src/main/java/ninja/oliverwerner/kassensystem/MainActivity.java
@@ -2,6 +2,13 @@ package ninja.oliverwerner.kassensystem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.GridView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +23,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void generateButtons(){
-        String[] buttonArray = queryButtons();
+        String[] buttonNameArray = queryButtons();
+        Button[] buttonArray = new Button[buttonNameArray.length];
+        GridView gridView = (GridView) findViewById(R.id.gridView);
+
+        for (int i = 0; i < buttonArray.length; i++){
+
+            Button btn = new Button(this);
+
+            btn.setText(buttonNameArray[i]);
+            btn.setLayoutParams(new GridLayout.LayoutParams());
+            btn.setMaxWidth(50);
+            btn.setMaxHeight(50);
+            btn.setPadding(10, 10, 10, 10);
+            gridView.addView(btn, i);
+        }
+
+        ArrayAdapter adapter = new ArrayAdapter(this,buttonArray);
+        return;
     }
 
     @Override
