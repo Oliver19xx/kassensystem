a77283a71b3a05e43356e64cd90eccf48283b02b
Oliver19xx <oliver.werner94@gmail.com>
2016-10-31 09:01:26 +0100

    - Funktion hinzugefügt: queryButton() - Funktion hinzugefügt: generateButtons()

--- app/src/main/java/ninja/oliverwerner/kassensystem/MainActivity.java
+++ app/src/main/java/ninja/oliverwerner/kassensystem/MainActivity.java
@@ -5,9 +5,24 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String[] queryButtons(){
+        String[] buttonArray = {
+                "Getränke",
+                "Vorspeisen",
+                "Hauptspeisen",
+                "Nachtisch"
+        };
+        return buttonArray;
+    }
+
+    private void generateButtons(){
+        String[] buttonArray = queryButtons();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        generateButtons();
     }
 }
